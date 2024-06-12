0f2e567b39ac31d2dec965bd45ab7012090808c0
Archit Garg <69807940+architgarg13@users.noreply.github.com>
2024-06-12 12:34:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,10 @@ gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017
 gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A | sudo tee /etc/apt/trusted.gpg.d/pritunl.asc
 sudo apt update
 sudo apt install pritunl-client
-pritunl-client add pritunl://pritunl.samajh.ai/ku/dKrerva4
+
+PRITUNL_PROFILE_URL="ADD URL HERE"
+
+pritunl-client add "$PRITUNL_PROFILE_URL"
 
 PROFILE_NAME=$(pritunl-client list | grep -oP '^\d+\. \K.*')
 # Output the profile name
