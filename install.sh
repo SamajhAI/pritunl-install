a49b07f149dc5a830536649e88d4717ad5970b7e
Archit Garg <69807940+architgarg13@users.noreply.github.com>
2024-06-12 10:21:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
-deb https://repo.pritunl.com/stable/apt mantic main
+deb https://repo.pritunl.com/stable/apt focal main
 EOF
 
 sudo apt --assume-yes install gnupg
-gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A | sudo tee /etc/apt/trusted.gpg.d/pritunl.asc
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys E162F504A20CDF15827F718D4B7C549A058F8B6B
+gpg --armor --export E162F504A20CDF15827F718D4B7C549A058F8B6B | sudo tee /etc/apt/trusted.gpg.d/pritunl.asc
 sudo apt update
 sudo apt install pritunl-client-electron
