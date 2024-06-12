064e974844e09b59d3476f6d43c997847f967f58
Archit Garg <69807940+architgarg13@users.noreply.github.com>
2024-06-12 11:42:27 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,14 @@ gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017
 gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A | sudo tee /etc/apt/trusted.gpg.d/pritunl.asc
 sudo apt update
 sudo apt install pritunl-client
+pritunl-client add pritunl://pritunl.samajh.ai/ku/dKrerva4
+
+PROFILE_NAME=$(pritunl-client list | grep -oP '^\d+\. \K.*')
+# Output the profile name
+echo "Profile Name: $PROFILE_NAME"
+
+# Start the VPN connection using the extracted profile name
+pritunl-client start "$PROFILE_NAME"
+
+# Verify the connection status
+pritunl-client status "$PROFILE_NAME"
